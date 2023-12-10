03fa69e1d40fb08c1588ed543b7bac0b6b49be28
Кирилл <105546265+KirrieD@users.noreply.github.com>
2023-12-10 18:26:38 +0400

    fix code

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static void main(String[] args) {
         int balance = 100; // Баланс
         int amount = 1100; // Пополнение
-        boolean bonus = amount >= 1_000; // Бонус на пополнение "Да/Нет"
-        int x; // Расчеты по самой сумме
-        if (bonus) {
-            x = 1;
+        boolean condition = amount >= 1_000; // Бонус на пополнение "Да/Нет"
+        int bonus; // Расчеты по самой сумме
+        if (condition) {
+            bonus = 1;
         } else {
-            x = 0;
+            bonus = 0;
         }
-        int a = amount * x / 100; // Расчет бонуса
-        int b = amount + balance + a; // Расчет баланса
-        System.out.println("Баланс : " + b);
+        int calculation = amount * bonus / 100; // Расчет бонуса
+        int result = amount + balance + calculation; // Расчет баланса
+        System.out.println("Баланс : " + result);
     }
 }
